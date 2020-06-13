2034405b1415b2651ce181057632fd7fba0b58fd
Peter Kellman <kellmanp@gmail.com>
2020-06-13 15:54:57 -0400

    update

---
+++ html5/ISMRM 2001 Minimum MR data workshop/data/slide12.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{100F9390-0495-4384-A2A0-DAB0FC34AEFB}'];loadHandler&&loadHandler(11, '<div id="spr0_5c558581"><div id="spr1_5c558581" class="kern slide"><img id="img0_5c558581" src="data/img1.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_5c558581" class="kern slide"></div></div>');})();
